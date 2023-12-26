b08263a0288a087f2f1780025758cead44506726
nikulinvrn <nikulin.vrn@bk.ru>
2023-12-26 19:41:43 +0300

    Удалил count-запросы. Pageable и сам отдает информацию о числе записей и количестве страниц.

--- src/main/java/ru/trainee/creditmanager/repository/BankRepository.java
+++ src/main/java/ru/trainee/creditmanager/repository/BankRepository.java
@@ -20,19 +20,12 @@ public interface BankRepository extends CommonRepository<Bank> {
     @Query("select c from Customer c join c.banks b where b.id = :id")
     Page<Customer> getBankCustomersById(UUID id, Pageable p);
 
-    @Query("select count(c) from Customer c join c.banks b where b.id = :id")
-    Long countCustomersByBank(UUID id);
 
     @Query("select ct from CreditType ct join ct.bank b where b.id = :id")
     Page<CreditType> getBankCreditTypesById(UUID id, Pageable p);
 
-    @Query("select count(ct) from CreditType ct join ct.bank b where b.id = :id")
-    Long countCreditTypesByBank(UUID id);
 
     @Query("select lo from LoanOffer lo join lo.bank b where b.id = :id")
     Page<LoanOffer> getBankLoanOffersById(UUID id, Pageable p);
-
-    @Query("select count(lo) from LoanOffer lo join lo.bank b where b.id = :id")
-    Long countLoanOffersByBank(UUID id);
 
 }
